c74c9f19823ec9f538679d325244208a00cbe547
sahilsingh7867 <sahilsingh7867@gmail.com>
2025-11-24 10:15:53 +0000

    Fix build error by adding 'use client' directive

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
